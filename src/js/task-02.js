f4caaa0eeb743fa21daf711a33453a70c37c69b3
Jerrssey <fankton@gmail.com>
2022-12-09 21:16:52 +0200

    Task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsUl = document.querySelector("#ingredients");
+
+ingredients.forEach((item) => {
+  const ulItem = document.createElement("li");
+  ulItem.textContent = item;
+  ulItem.classList.add("item");
+  ingredientsUl.append(ulItem);
+});
