06cf3f55c896898ec2939aa02e09cf78cd7b33e9
Jerrssey <fankton@gmail.com>
2022-12-10 12:13:03 +0200

    Task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = document.querySelector(".widget");
+const colorField = refs.querySelector(".color");
+const changeColorBtn = refs.querySelector(".change-color");
+
+
+changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  colorField.textContent = randomColor;
+  document.body.style.backgroundColor = `${randomColor}`;
+});
