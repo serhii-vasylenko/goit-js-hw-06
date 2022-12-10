ac2548ac750a05c47c2d3b237cf332cfdb10e1a2
Jerrssey <fankton@gmail.com>
2022-12-10 15:01:11 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -3,14 +3,12 @@ const loginForm = document.querySelector(".login-form");
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const target = event.currentTarget;
-  const formData = {
-    email: target.email.value,
-    password: target.password.value,
-  };
+  const { email, password } = event.currentTarget.elements;
 
-  formData.email && formData.password
-    ? console.log(formData)
-    : alert("Усі поля мають бути заповненми!");
-  target.reset();
+  if (email.value && password.value) {
+    console.log({ email: email.value, password: password.value });
+    event.currentTarget.reset();
+  } else {
+    alert("Усі поля мають бути заповненми!");
+  }
 });
