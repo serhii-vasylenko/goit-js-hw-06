e87e2abcb9ce68124f853cb365fbb0739285f711
Jerrssey <fankton@gmail.com>
2022-12-10 12:05:18 +0200

    Task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const target = event.currentTarget;
+  const formData = {
+    email: target.email.value,
+    password: target.password.value,
+  };
+
+  formData.email && formData.password
+    ? console.log(formData)
+    : alert("Усі поля мають бути заповненми!");
+  target.reset();
+});
