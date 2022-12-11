f2643f5955de7d263c5cd9e2d32071a9c5098071
Jerrssey <fankton@gmail.com>
2022-12-11 15:06:49 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -2,7 +2,7 @@ const validationInput = document.querySelector("#validation-input");
 
 validationInput.addEventListener("blur", ({ currentTarget }) => {
 
-  currentTarget.value.length >= Number(validationInput.dataset.length)
+  currentTarget.value.length === Number(validationInput.dataset.length)
     ? validationInput.classList.add("valid")
     : validationInput.classList.add("invalid");
 });
