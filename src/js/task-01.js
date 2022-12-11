885d3c37a910bbc6a88f6da17ba3e04d7192b4fb
Jerrssey <fankton@gmail.com>
2022-12-11 15:56:20 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,6 +2,6 @@ const refs = document.querySelector("#categories");
 console.log(`Number of categories: ${refs.children.length}\n\n`);
 
 refs.querySelectorAll(".item").forEach((item) => {
-  console.log(`Category: ${item.querySelector("h2").textContent}`);
-  console.log(`Elements: ${item.querySelector("ul").children.length}\n\n`);
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}\n\n`);
 });
